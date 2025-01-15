239cb47c4737c8fc0f48e446d5497f0baff9390f
swngr <ngrshay@gmail.com>
2025-01-15 14:46:49 -0600

    Addition of negative test cases

--- src/test/java/APIEndpointsTest.java
+++ src/test/java/APIEndpointsTest.java
@@ -1,5 +1,7 @@
 
 import api.endpoints.Routes;
+import api.payload.BookingPayload;
+import api.payload.Guest;
 import api.payload.PropertyPayload;
 import io.restassured.RestAssured;
 import io.restassured.http.ContentType;
@@ -11,6 +13,7 @@ import io.restassured.http.Headers;
 
 
 import java.util.Arrays;
+import java.util.UUID;
 
 import static io.restassured.RestAssured.given;
 
@@ -26,19 +29,20 @@ public class APIEndpointsTest {
     }
 
     /**
-     * Validate that it's not possible to use GET property endpoint with wrong credentials
+     * Unable to access GET property endpoint due to wrong credentials
      */
     @Test
     public void testGetProperty_Unauthorized() {
         String username = "wrong_username";
         String password = "wrong_password";
         // Get authentication headers
+        //Method overloading
         Headers headers = BasicAuthentication.getBasicAuthHeaders(username, password);
         // Perform GET request
         Response response = given()
                 .headers(headers)
                 .when()
-                .get("/properties")
+                .get(Routes.properties_endpoint)
                 .then()
                 .extract()
                 .response();
@@ -48,7 +52,7 @@ public class APIEndpointsTest {
     }
 
     /**
-     * Validate that it's not possible to use POST property endpoint with wrong credentials
+     * Unable to access POST property endpoint due to wrong credentials
      */
     @Test
     public void testPostProperty_Unauthorized() {
@@ -60,7 +64,7 @@ public class APIEndpointsTest {
         Response response = given()
                 .headers(headers)
                 .when()
-                .get("/properties")
+                .get(Routes.properties_endpoint)
                 .then()
                 .extract()
                 .response();
@@ -71,7 +75,7 @@ public class APIEndpointsTest {
     }
 
     /**
-     * Validate that it's not possible to use GET bookings endpoint with wrong credentials
+     * Unable to access GET bookings endpoint due to wrong credentials
      */
     @Test
     public void testGetBookings_Unauthorized() {
@@ -83,7 +87,7 @@ public class APIEndpointsTest {
         Response response = given()
                 .headers(headers)
                 .when()
-                .get("/bookings")
+                .get(Routes.bookings_endpoint)
                 .then()
                 .extract()
                 .response();
@@ -93,7 +97,7 @@ public class APIEndpointsTest {
     }
 
     /**
-     * Validate that it's not possible to use POST bookings endpoint with wrong credentials
+     * Unable to access POST bookings endpoint due to wrong credentials
      */
     @Test
     public void testPostBookings_Unauthorized() {
@@ -105,7 +109,7 @@ public class APIEndpointsTest {
         Response response = given()
                 .headers(headers)
                 .when()
-                .get("/bookings")
+                .get(Routes.bookings_endpoint)
                 .then()
                 .extract()
                 .response();
@@ -116,7 +120,7 @@ public class APIEndpointsTest {
     }
 
     /**
-     * Validate that GET property endpoint is working fine
+     * Successful access to GET property endpoint
      */
     @Test
     public void testGetProperties() {
@@ -126,7 +130,7 @@ public class APIEndpointsTest {
         Response response = given()
                 .headers(headers)
                 .when()
-                .get("/properties")
+                .get(Routes.properties_endpoint)
                 .then()
                 .extract()
                 .response();
@@ -136,21 +140,20 @@ public class APIEndpointsTest {
     }
 
     /**
-     * Validate that POST property endpoint is working fine
+     * Functional Tests
+     * Property Creation
+     * Successful access to POST property endpoint
      */
     @Test
     public void testPostProperties_Valid() {
         // Get authentication headers
         Headers headers = BasicAuthentication.getBasicAuthHeaders();
-
         // Define request body using POJO class
         PropertyPayload requestBody = new PropertyPayload();
-
-        requestBody.setId("f981cd14-e0b5-41bd-a612-d53cf8aa1111");
-        requestBody.setAlias("sng_testprop25");
+        requestBody.setId(UUID.randomUUID().toString());
+        requestBody.setAlias("sng_testprop27");
         requestBody.setCountryCode("1");
         requestBody.setCreatedAt(Arrays.asList(2024, 1, 14, 20, 3, 7, 253493000L));
-
         Response response = RestAssured
                 .given()
                 .headers(headers)
@@ -159,43 +162,32 @@ public class APIEndpointsTest {
                 .body(requestBody) // Serialize POJO to JSON
                 .when()
                 .post(Routes.properties_endpoint);
-
         // Log the response
         response.then().log().all();
-
         // Assert that the status code is 201
         Assert.assertEquals(response.getStatusCode(), 201, "Status code is not as expected!");
     }
 
     /**
-     * Validate short Id number
+     * Negative test case: short property Id number
      */
     @Test
     public void testPostProperties_shortId_Invalid() {
         // Get authentication headers
         Headers headers = BasicAuthentication.getBasicAuthHeaders();
         // Define request body
-        String requestBody = "{\n" +
-                "\"id\": \"f981cd14-e0b5-41bd-a612-\",\n" +
-                "        \"alias\": \"sng_testprop17\",\n" +
-                "        \"countryCode\": 1,\n" +
-                "        \"createdAt\": [\n" +
-                "            2024,\n" +
-                "            1,\n" +
-                "            13,\n" +
-                "            17,\n" +
-                "            47,\n" +
-                "            16,\n" +
-                "            858649000\n" +
-                "        ] \n" +
-                "}";
+        PropertyPayload requestBody = new PropertyPayload();
+        requestBody.setId("f981cd14-e0b5-41bd-a612-");
+        requestBody.setAlias("sng_testprop27");
+        requestBody.setCountryCode("1");
+        requestBody.setCreatedAt(Arrays.asList(2024, 1, 14, 20, 3, 7, 253493000L));
         Response response = given()
                 .headers(headers)
                 .contentType(ContentType.JSON)
                 .accept(ContentType.JSON)
                 .body(requestBody)
                 .when()
-                .post("/properties");
+                .post(Routes.properties_endpoint);
         response.then().log().all();
         // Assertions
         Assert.assertEquals(response.getStatusCode(), 400, "Status code should be 400");
@@ -203,106 +195,86 @@ public class APIEndpointsTest {
     }
 
     /**
-     * Validate duplicated Alias
+     * Negative test case: duplicated Alias
      */
     @Test
     public void testPostProperties_duplicatedAlias_Invalid() {
         // Get authentication headers
         Headers headers = BasicAuthentication.getBasicAuthHeaders();
         // Define request body
-        String requestBody = "{\n" +
-                "\"id\": \"f981cd14-e0b5-41bd-a612-d53cf8aa1111\",\n" +
-                "        \"alias\": \"sng_testprop17\",\n" +
-                "        \"countryCode\": 1,\n" +
-                "        \"createdAt\": [\n" +
-                "            2024,\n" +
-                "            1,\n" +
-                "            13,\n" +
-                "            17,\n" +
-                "            47,\n" +
-                "            16,\n" +
-                "            858649000\n" +
-                "        ] \n" +
-                "}";
+        PropertyPayload requestBody = new PropertyPayload();
+        requestBody.setId(UUID.randomUUID().toString());
+        requestBody.setAlias("sng_testprop15");
+        requestBody.setCountryCode("1");
+        requestBody.setCreatedAt(Arrays.asList(2024, 1, 14, 20, 3, 7, 253493000L));
         Response response = given()
                 .headers(headers)
                 .contentType(ContentType.JSON)
                 .accept(ContentType.JSON)
                 .body(requestBody)
                 .when()
-                .post("/properties");
+                .post(Routes.properties_endpoint);
         response.then().log().all();
         // Assertions
-        Assert.assertEquals(response.getStatusCode(), 400, "Status code should be 400");
+        Assert.assertEquals(response.getStatusCode(), 500, "Status code should be 500");
         System.out.println("POST Response: " + response.asString());
     }
 
     /**
-     * Validate empty Country Code
+     * Negative test case: empty Country Code
      */
     @Test
     public void testPostProperties_EmptyCountryCode_Invalid() {
         // Get authentication headers
         Headers headers = BasicAuthentication.getBasicAuthHeaders();
         // Define request body
-        String requestBody = "{\n" +
-                "\"id\": \"f981cd14-e0b5-41bd-a612-d53cf8aa1111\",\n" +
-                "        \"alias\": \"sng_testprop25\",\n" +
-                "        \"countryCode\": \"\",\n" +
-                "        \"createdAt\": [\n" +
-                "            2024,\n" +
-                "            1,\n" +
-                "            13,\n" +
-                "            17,\n" +
-                "            47,\n" +
-                "            16,\n" +
-                "            858649000\n" +
-                "        ] \n" +
-                "}";
-
+        PropertyPayload requestBody = new PropertyPayload();
+        requestBody.setId(UUID.randomUUID().toString());
+        requestBody.setAlias("sng_testprop30");
+        requestBody.setCountryCode(null);
+        requestBody.setCreatedAt(Arrays.asList(2024, 1, 14, 20, 3, 7, 253493000L));
         Response response = given()
                 .headers(headers)
                 .contentType(ContentType.JSON)
                 .accept(ContentType.JSON)
                 .body(requestBody)
                 .when()
-                .post("/properties");
+                .post(Routes.properties_endpoint);
         response.then().log().all();
         // Assertions
-        Assert.assertEquals(response.getStatusCode(), 400, "Country Code should not be empty. Status code should be 400");
+        Assert.assertEquals(response.getStatusCode(), 400, "Country Code should not be empty. Status code not as expected");
         System.out.println("POST Response: " + response.asString());
     }
 
     /**
-     * Validate empty Created At date
+     * Negative test case: Created At date in the future
      */
     @Test
     public void testPostProperties_EmptyCreatedAt_Invalid() {
+
         // Get authentication headers
         Headers headers = BasicAuthentication.getBasicAuthHeaders();
         // Define request body
-        String requestBody = "{\n" +
-                "\"id\": \"f981cd14-e0b5-41bd-a612-d53cf8aa1111\",\n" +
-                "        \"alias\": \"sng_testprop26\",\n" +
-                "        \"countryCode\": 1,\n" +
-                "        \"createdAt\": [] \n" +
-                "}";
-
+        PropertyPayload requestBody = new PropertyPayload();
+        requestBody.setId(UUID.randomUUID().toString());
+        requestBody.setAlias("sng_testprop27");
+        requestBody.setCountryCode(null);
+        requestBody.setCreatedAt(Arrays.asList(2035, 1, 14, 20, 3, 7, 253493000L));
         Response response = given()
                 .headers(headers)
                 .contentType(ContentType.JSON)
                 .accept(ContentType.JSON)
                 .body(requestBody)
                 .when()
-                .post("/properties");
+                .post(Routes.properties_endpoint);
         response.then().log().all();
         // Assertions
-        Assert.assertEquals(response.getStatusCode(), 400, "Crated At: should not be empty. Status code should be 400");
+        Assert.assertEquals(response.getStatusCode(), 400, "Crated At date: should not be in the future. Status code is not as expected");
         System.out.println("POST Response: " + response.asString());
     }
 
     /**
-     * Validate empty body
+     * Negative test case: empty body
      */
     @Test
     public void testPostProperties_EmptyBody_Invalid() {
@@ -311,14 +283,13 @@ public class APIEndpointsTest {
         // Define request body
         String requestBody = "{\n" +
                 "}";
-
         Response response = given()
                 .headers(headers)
                 .contentType(ContentType.JSON)
                 .accept(ContentType.JSON)
                 .body(requestBody)
                 .when()
-                .post("/properties");
+                .post(Routes.properties_endpoint);
         response.then().log().all();
         // Assertions
         Assert.assertEquals(response.getStatusCode(), 400, "Status code should be 400");
@@ -326,7 +297,7 @@ public class APIEndpointsTest {
     }
 
     /**
-     * Validate that GET bookings endpoint is working fine
+     * Successful access to GET bookings endpoint
      */
     @Test
     public void testGetBookings() {
@@ -336,7 +307,7 @@ public class APIEndpointsTest {
         Response response = given()
                 .headers(headers)
                 .when()
-                .get("/bookings")
+                .get(Routes.bookings_endpoint)
                 .then()
                 .extract()
                 .response();
@@ -346,80 +317,106 @@ public class APIEndpointsTest {
     }
 
     /**
-     * Validate that POST bookings endpoint is working fine
+     * Functional Tests
+     * Booking creation -> SUCCESSFUL
      */
     @Test
     public void testPostBookings() {
         // Get authentication headers
         Headers headers = BasicAuthentication.getBasicAuthHeaders();
-        // Define request body
-        String requestBody = "{\n" +
-                "\"id\": \"3fa85f64-5717-4562-b3fc-2c963f66afa6\",\n" +
-                "  \"startDate\": \"2024-01-15\",\n" +
-                "  \"endDate\": \"2024-01-15\",\n" +
-                "  \"status\": \"SCHEDULED\",\n" +
-                "  \"guest\": {\n" +
-                "    \"firstName\": \"swngr\",\n" +
-                "    \"lastName\": \"swngrLast\",\n" +
-                "    \"dateOfBirth\": \"2025-01-14\"\n" +
-                "  },\n" +
-                "  \"propertyId\": \"ff288cc0-c049-4bc4-a96d-01637466bed4\"\n" +
-                "}";
-        Response response = given()
-                .headers(headers)
-                .contentType(ContentType.JSON)
-                .accept(ContentType.JSON)
-                .body(requestBody)
-                .when()
-                .post("/bookings");
-        response.then().log().all();
-        // Assertions
-        Assert.assertEquals(response.getStatusCode(), 201, "Status code should be 201");
-        System.out.println("POST Response: " + response.asString());
-    }
-
-
-    /**
-     * Property Retrieval
-     */
-    @Test
-    public void storePropertyData() {
-        // Get authentication headers
-        Headers headers = BasicAuthentication.getBasicAuthHeaders();
-
-        // Define request body using POJO class
-        PropertyPayload requestBody = new PropertyPayload();
-
-        requestBody.setId("f981cd14-e0b5-41bd-a612-d53cf8aa1111");
-        requestBody.setAlias("sng_testprop25");
-        requestBody.setCountryCode("1");
-        requestBody.setCreatedAt(Arrays.asList(2024, 1, 14, 20, 3, 7, 253493000L));
-
+        // Creating the request body
+        BookingPayload bookingPayload = new BookingPayload();
+        bookingPayload.setId(UUID.randomUUID().toString());
+        bookingPayload.setStartDate("2023-01-02");
+        bookingPayload.setEndDate("2023-01-02");
+        bookingPayload.setStatus("SCHEDULED");
+        bookingPayload.setPropertyId("ff288cc0-c049-4bc4-a96d-01637466bed4"); //-->sng_testprop15
+        // Creating the guest object
+        Guest guest = new Guest();
+        guest.setFirstName("Sw");
+        guest.setLastName("Ngr");
+        guest.setDateOfBirth("2025-01-14");
+        // Setting the guest object in the request
+        bookingPayload.setGuest(guest);
         Response response = RestAssured
                 .given()
                 .headers(headers)
                 .contentType(ContentType.JSON)
                 .accept(ContentType.JSON)
-                .body(requestBody) // Serialize POJO to JSON
+                .body(bookingPayload) // Serialize POJO to JSON
                 .when()
-                .post(Routes.properties_endpoint);
-
+                .post(Routes.bookings_endpoint);
         // Log the response
         response.then().log().all();
-
         // Assert that the status code is 201
         Assert.assertEquals(response.getStatusCode(), 201, "Status code is not as expected!");
+    }
 
-        // Assert specific field values in the response body
+    /**
+     * Functional Tests
+     * Property Retrieval
+     */
+    @Test
+    public void getPropertyData() {
+        // Get authentication headers
+        Headers headers = BasicAuthentication.getBasicAuthHeaders();
+        // Perform GET request
+        Response response = given()
+                .headers(headers)
+                .when()
+                .get(Routes.properties_endpoint + "/ff288cc0-c049-4bc4-a96d-01637466bed4")
+                .then()
+                .extract()
+                .response();
+        // Assertions
+        Assert.assertEquals(response.getStatusCode(), 200, "Status code should be 200");
+        System.out.println("GET Response: " + response.asString());
+        //Assert specific field values in the response body
         String propertyId = response.jsonPath().getString("id");
         String propertyAlias = response.jsonPath().getString("alias");
-
         Assert.assertNotEquals(propertyId, "", "id should not be empty");
         Assert.assertNotNull(propertyId, "id should not be null");
         Assert.assertNotEquals(propertyAlias, "", "alias should not be empty");
         Assert.assertNotNull(propertyAlias, "alias should not be null");
 
+        System.out.println("Response id: " + propertyId);
         System.out.println("Response Alias: " + propertyAlias);
+    }
+
+    /**
+     * Functional Tests
+     * Booking creation -> UNSUCCESSFUL due to overlapped days
+     */
+    @Test
+    public void testPostBookings_Overlap() {
+        // Get authentication headers
+        Headers headers = BasicAuthentication.getBasicAuthHeaders();
+        // Creating the request body
+        BookingPayload bookingPayload = new BookingPayload();
+        bookingPayload.setId(UUID.randomUUID().toString());
+        bookingPayload.setStartDate("2023-01-02");
+        bookingPayload.setEndDate("2023-01-02");
+        bookingPayload.setStatus("SCHEDULED");
+        bookingPayload.setPropertyId("ff288cc0-c049-4bc4-a96d-01637466bed4"); //-->sng_testprop15
+        // Creating the guest object
+        Guest guest = new Guest();
+        guest.setFirstName("Sw");
+        guest.setLastName("Ngr");
+        guest.setDateOfBirth("2025-01-14");
+        // Setting the guest object in the request
+        bookingPayload.setGuest(guest);
+        Response response = RestAssured
+                .given()
+                .headers(headers)
+                .contentType(ContentType.JSON)
+                .accept(ContentType.JSON)
+                .body(bookingPayload) // Serialize POJO to JSON
+                .when()
+                .post(Routes.bookings_endpoint);
+        // Log the response
+        response.then().log().all();
+        // Assert that the status code is 201
+        Assert.assertEquals(response.getStatusCode(), 422, "Status code is not as expected!");
     }
 
 }
