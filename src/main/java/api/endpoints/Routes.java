f2d0a3f0ca34329b8854bbc9c4034449cdbd94e4
swngr <ngrshay@gmail.com>
2025-01-15 01:20:05 -0600

    Redefined /properties and /bookings endpoints

--- src/main/java/api/endpoints/Routes.java
+++ src/main/java/api/endpoints/Routes.java
@@ -5,12 +5,10 @@ public class Routes {
     public static String base_url = "https://qa-assessment.svc.hostfully.com";
 
     //properties
-    public static String properties_get_url = base_url + "/properties";
-    public static String properties_post_url = base_url + "/properties";
+    public static String properties_endpoint = "/properties";
 
     //booking
-    public static String booking_get_url = base_url + "/bookings";
-    public static String booking_post_url = base_url + "/bookings";
+    public static String bookings_endpoint = "/bookings";
 
 
 }
